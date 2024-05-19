9c5e7e26318a5b391354a8473d83e5a8de9d962e
kunjkk <102272412+kunjkk@users.noreply.github.com>
2024-05-19 16:45:01 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const pool = require("./db")
+app.use(cors())
+app.use(express.json())
+
+app.post("/todos" , async (req,res) => {
+    try{
+        // console.log(req.body);
+        const { description} = req.body
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description])
+        res.json(newTodo.rows)
+    }catch(err) {
+        console.error(err)
+        console.log(err);
+    }
+})
+
+// get all todos
+
+app.get("/todos", async (req , res)=>{
+    try {
+      const allTodos = await pool.query("SELECT * FROM todo")  
+        res.json(allTodos.rows)
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+// get a todo
+app.get("/todos/:id" , async (req,res) =>{
+    try {
+        const {id} = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id] )
+        res.json(todo.rows)
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+//update a todo 
+
+app.put("/todos/:id" , async(req,res) => {
+    try {
+        const {id} = req.params
+        const {description} = req.body
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",[description ,id])
+        res.json(   "Todo was updated!")
+    } catch (error) {
+        
+    }
+})
+
+app.delete("/todos/:id", async (req,res) =>{
+    try {
+        const {id}  = req.params
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",[
+            id
+        ])
+        console.log(deleteTodo);
+        res.json("Todo was deleted!")
+    } catch (err) {
+        console.log(err);
+    }
+})
+app.listen(5007, () =>{
+    console.log("server has started on port 5007");
+})
